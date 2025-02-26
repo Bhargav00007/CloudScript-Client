d2e3e1eaba071f28967e14b47496a56ba0523507
Bhargav Pattanayak <122164885+Bhargav00007@users.noreply.github.com>
2025-02-27 01:21:46 +0530

    Latest backend link (Atyab's link)

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,16 +7,19 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://bhrgvnotesbackend.onrender.com/ ", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
+    const response = await fetch(
+      "https://bhrgvnotesbackend.onrender.com/api/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      }
+    );
     const json = await response.json();
     console.log(json);
     if (json.success) {
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -12,13 +12,16 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { name, email, password } = credentials;
-    const response = await fetch("https://bhrgvnotesbackend.onrender.com/ ", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
+    const response = await fetch(
+      "https://bhrgvnotesbackend.onrender.com/api/auth/createuser ",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      }
+    );
     const json = await response.json();
     console.log(json);
     if (json.success) {
